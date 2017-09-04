2086ea477db356b95b54175261a0ea1a4054f710
David Wong <wongcheming@gmail.com>
2017-09-04 23:39:19 +0100

    Added model testing

--- test/client/model.js
+++ test/client/model.js
@@ -1,6 +1,12 @@
-var chai = require("chai");
+import chai from 'chai';
 chai.should();
-var model = require("../../lib/client/hotels/model.js");
+var model = require('../../lib/client/hotels/model.js');
+import R from 'ramda';
+import React from 'react';
+import SearchInstructions from '../../lib/client/hotels/SearchInstructions.js';
+import sinon from 'sinon';
+import Task from 'data.task';
+import Either from 'data.either';
 
 describe("Given I have a one filter and sort ascending", function() {
   it("should create a query with one facility and sort ascending", function() {
@@ -11,6 +17,7 @@ describe("Given I have a one filter and sort ascending", function() {
   
   })
 });
+
 describe("Given I have a two filters and sort ascending", function() {
   it("should create a query with two facilities and sort ascending", function() {
 
@@ -18,6 +25,32 @@ describe("Given I have a two filters and sort ascending", function() {
 
     result.should.equal("?facilities=Pool&facilities=Car+Park&sort=1");
   
+  })
+});
+
+describe("Given I have searched for hotels with no results", function() {
+  it("should give me instructions", function() {
+
+    const result = model.searchHotels("");
+    const either = result.fork(R.identity, R.identity);
+    const instructions = either.cata({ Left: R.identity });
+
+    instructions.should.deep.equal(<SearchInstructions/>);
+  })
+});
+
+describe("Given I have searched for hotels with results", function() {
+  it("should give me data", function() {
+
+    const data = {"items":[{"id":6,"Name":"hotel6","StarRating":3,"Facilities":["pool"]}]};
+
+    const t1 = Task.of(Either.Right(data)); 
+    const t2 = R.map(R.map(model.toHotels))(t1); 
+
+    const either = t2.fork(R.identity, R.identity);
+    const hotels = either.cata({ Right: R.identity });
+
+    hotels[0].Name.should.equal("hotel6");
   })
 });
 
