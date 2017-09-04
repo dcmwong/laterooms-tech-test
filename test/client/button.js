aeffcbb04689ce9f6140d0c54dedafee50d49f66
David Wong <wongcheming@gmail.com>
2017-09-04 22:18:29 +0100

    Added more data

--- test/client/button.js
+++ test/client/button.js
@@ -1,19 +1,28 @@
-var chai = require("chai");
+import chai from 'chai'; 
 chai.should();
-// import {mount, render, shallow} from 'enzyme';
+import {mount, render, shallow} from 'enzyme';
+import React from 'react';
+import CarPark from 'react-icons/lib/md/directions-car';
+import CarParkButton from '../../lib/client/hotels/buttons/CarPark.js';
+import sinon from 'sinon';
 
-var CarParkButton = require("../../lib/client/hotels/buttons/CarPark.js");
-
-describe.skip("<CarParkButton />", function() {
+describe("<CarParkButton />", function() {
   it("should toggle between on and off states", function() {
 
-    const wrapper = shallow(<Checkbox />);
+    const addAndSubmitFn = sinon.spy(); 
+    const negateAndSubmitFn = sinon.spy(); 
 
-    wrapper.find('CarPark').props()['color'].should.equal("white");
+    const wrapper = shallow(<CarParkButton 
+      addAndSubmit={addAndSubmitFn} 
+      negateAndSubmit={negateAndSubmitFn} />);
 
     wrapper.find('a').simulate('click');
+    wrapper.find(CarPark).props().color.should.equal('gold');
+    addAndSubmitFn.calledOnce.should.equal(true);
 
-    wrapper.find('CarPark').props()['color'].should.equal("red");
+    wrapper.find('a').simulate('click');
+    wrapper.find(CarPark).props().color.should.equal('white');
+    negateAndSubmitFn.calledOnce.should.equal(true);
   })
 });
 
